f6db32b979b835136eebc2cfbfa4d3486c8c5d26
Era <amirb2467@gmail.com>
2021-12-11 03:04:23 -0800

    added API data for current day, and 5 day forecast. Data now displays on webpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,28 @@
 var userInput = [];
+var userCity;
+
+//creates variable for each day
+var today = new Date();
+var day1 = new Date(today);
+var day2 = new Date(today);
+var day3 = new Date(today);
+var day4 = new Date(today);
+var day5 = new Date(today);
+//sets variables to the following 5 days
+day1.setDate(day1.getDate() + 1);
+day2.setDate(day2.getDate() + 2);
+day3.setDate(day3.getDate() + 3);
+day4.setDate(day4.getDate() + 4);
+day5.setDate(day5.getDate() + 5);
+//sets the variables as a string
+today = today.toLocaleDateString();
+day1 = day1.toLocaleDateString();
+day2 = day2.toLocaleDateString();
+day3 = day3.toLocaleDateString();
+day4 = day4.toLocaleDateString();
+day5 = day5.toLocaleDateString();
+//puts days into an array for easier calling in funcitons
+var days = [day1, day2, day3, day4, day5];
 
 //gets cities lat and long to be used for weather API call
 var getCoord = function(city) {
@@ -7,6 +31,7 @@ var getCoord = function(city) {
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
+                userCity = city;
                 getApiFetch(data.coord.lat, data.coord.lon);
             });
         } else {
@@ -23,6 +48,8 @@ var getApiFetch = function(lat, long) {
         if (response.ok) {
             response.json().then(function(data) {
                 console.log(data);
+                displayMainForecast(data.current);
+                displaySubForecasts(data.daily);
             });
         } else {
             alert("something went wrong while fetching api");
@@ -30,12 +57,54 @@ var getApiFetch = function(lat, long) {
     });
 };
 
+//displays weather info for today
 var displayMainForecast = function(data) {
-
+    //adds the weather info to HTML elements
+    $("#weather-main").find("#city").text(userCity + "(" + today +")" + getWeatherIcon(data));
+    $("#weather-main").find("#temp").text("Temp: " + data.temp + "°F");
+    $("#weather-main").find("#wind").text("Wind: " + data.wind_speed + " MPH");
+    $("#weather-main").find("#humidity").text("Humidity: " + data.humidity + "%");
+    
+    //if loop sets color of UV index according to severity
+    if(data.uvi <= 2) {
+        $("#weather-main").find("#uv").addClass("bg-success").text(data.uvi);
+    } else if (data.uvi <= 5) {
+        $("#weather-main").find("#uv").addClass("bg-warning").text(data.uvi);
+    } else if (data.uvi <= 7) {
+        $("#weather-main").find("#uv").addClass("bg-danger").text(data.uvi);
+    } else {
+        $("#weather-main").find("#uv").addClass("bg-secondary").text(data.uvi);
+    };
 };
 
+//displays weather info for the following 5 days
 var displaySubForecasts = function(data) {
+    console.log(data);
+    for(var i=1; i<=5; i++){
+        $("#day" + i).find("#date").text("(" + days[i-1] +")");
+        $("#day" + i).find("#icon").text(getWeatherIcon(data[i-1]));
+        $("#day" + i).find("#temp").text("Temp: " + data[i-1].temp.day + "°F");
+        $("#day" + i).find("#wind").text("Wind: " + data[i-1].wind_speed + " MPH");
+        $("#day" + i).find("#humidity").text("Humidity: " + data[i-1].humidity + "%");
+    }
+};
 
+//gets the appropriate icon for each weather type
+var getWeatherIcon = function(data) {
+    var weather = data.weather[0].main;
+    if (weather === 'Clear') {
+        return "☀️";
+    } else if (weather === 'Clouds') {
+        return "☁️";
+    } else if (weather  === 'Rain' || weather === 'Drizzle') {
+        return "🌧️";
+    } else if (weather === 'Thunderstorm') {
+        return "⛈️";
+    } else if (weather === 'Snow') {
+        return "❄️";
+    } else {
+        return "⛅";
+    }
 };
 
 //loads in the cities when the site first loads from localStorage
